df3c5cf4d1886aa40fb7ae6710219e10e7b74e73
Behzad-Az <behy1368@gmail.com>
2024-03-02 21:12:36 -0800

    finished project section

--- components/section-header.tsx
+++ components/section-header.tsx
@@ -6,6 +6,6 @@ type Props = {
 
 export default function SectionHeader({ children }: Props) {
   return (
-    <h2 className="text-3xl font-medium capitalize mb-8">{children}</h2>
+    <h2 className="text-3xl font-medium capitalize mb-8 text-center">{children}</h2>
   );
 }
