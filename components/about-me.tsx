d929d252e2c47f344fe11e92e5c218691e28975f
Behzad-Az <behy1368@gmail.com>
2024-03-02 21:41:11 -0800

    working on navbar

--- components/about-me.tsx
+++ components/about-me.tsx
@@ -6,7 +6,8 @@ import { motion } from "framer-motion";
 export default function AboutMe() {
   return (
     <motion.section 
-      className="max-w-[45rem] text-center leading-7"
+      id="about"
+      className="max-w-[45rem] text-center leading-7 scroll-mt-28"
       initial={{ opacity: 0, y: 100 }}
       animate={{ opacity: 1, y: 0 }}
       transition={{ delay: 0.175 }}
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,11 +1,13 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
 import { motion } from "framer-motion";
 import { links } from "@/lib/data";
 import Link from "next/link";
+import clsx from "clsx";
 
 export default function Navbar() {
+  const [activeSection, setActiveSection] = useState("Home");
   return (
     <header className="z-[999] relative">
       <motion.div className="fixed top-0 left-1/2 h-[4.5rem] w-full rounded-none border border-white border-opacity-40 bg-white bg-opacity-80 shadow-lg shadow-black/[0.03] backdrop-blur-[0.5rem] sm:top-6 sm:h-[3.25rem] sm:w-[36rem] sm:rounded-full"
@@ -18,11 +20,34 @@ export default function Navbar() {
             links.map(link => (
               <motion.li 
                 key={link.hash} 
-                className="h-3/4 flex items-center justify-center"
+                className="h-3/4 flex items-center justify-center relative"
                 initial={{ y: -100, opacity: 0 }}
                 animate={{ y: 0, opacity: 1 }}
               >
-                <Link href={link.hash} className="flex w-full items-center justify-center px-3 py-3 hover:text-gray-950 transition">{link.name}</Link>
+                <Link 
+                  className={clsx(
+                    "flex w-full items-center justify-center px-3 py-3 hover:text-gray-950 transition rounded-full", 
+                    {
+                      "text-gray-950": activeSection === link.name,
+                    }
+                  )}
+                  href={link.hash}
+                  onClick={() => setActiveSection(link.name)}
+                >
+                  {link.name}
+                  {
+                    activeSection === link.name &&
+                    <motion.span 
+                      className="bg-gray-100 rounded-full absolute inset-0 -z-10" 
+                      layoutId="activeSection"
+                      transition={{
+                        type: "spring",
+                        stiffness: 380,
+                        damping: 30
+                      }}
+                    />
+                  }
+                </Link>
               </motion.li>
             ))
           }
